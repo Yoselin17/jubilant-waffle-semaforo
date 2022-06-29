145e3f4fc5af30ee6c97a689d989ee267091f435
yoselin retamal <yoselin_yar@hotmail.com>
2022-06-29 16:10:58 -0400

    semaforo

--- src/Semaforo.js
+++ src/Semaforo.js
@@ -4,24 +4,55 @@ import { useState } from "react";
 
 const Semaforo = () => {
 
-    const [color, setColor] = useState("yellow");
+    const [uno, setUno] = useState(false);
+    const [dos, setDos] = useState(false);
+    const [tres, setTres] = useState(false);
 
-    console.log(color)
+    const cambiaColor = (a) => {
+
+        if (a.target.className == "circulos3") {
+            setUno(true)
+            setDos(false)
+            setTres(false)
+        }
+
+        else if (a.target.className == "circulos2") {
+            setUno(false)
+            setDos(true)
+            setTres(false)
+
+
+        }
+        else if (a.target.className == "circulos1") {
+            setUno(false)
+            setDos(false)
+            setTres(true)
+
+        }
+
+
+
+    }
 
     return (
 
         <div className="container">
             <div className="semaforo">
-                <span className="circulos3" onClick={() => {
-                    setColor("red")
+                    <span className={uno==true?"circulos3 brillanteRed":"circulos3"}  onClick={(e) => {
+                    cambiaColor(e)
+                    console.log("red")
+
                 }}></span>
-                <span className="circulos2" onClick={() => {
-                    setColor("yellow")
+                <span className={dos==true? "circulos2 brillanteYellow":"circulos2"} onClick={(e) => {
+                    cambiaColor(e)
                 }}></span>
-                <span className="circulos1" onClick={() => {
-                    setColor("green")
+                <span className={tres==true?"circulos1 brillanteGreen":"circulos1"} onClick={(e) => {
+                    cambiaColor(e)
                 }}></span>
+
+                <div className="sombreado"></div>
             </div>
+
         </div>
     )
 }
